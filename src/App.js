1850d753cf6654d209631c41c084962d529b87cd
scotch115 <jordangamache115@gmail.com>
2020-03-26 13:53:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Webcam from 'react-webcam';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	enableWebcam = () => this.setState({ webcamEnabled: true });
+	switchCamera = () => {
+		if (this.state.facingMode === "user") {
+			this.setState({ facingMode: "environment" });
+		} else if (this.state.facingMode === "environment") {
+			this.setState({ facingMode: "user" });
+		}
+	}
+
+	// webcamRef = () => React.useRef(null);
+	// capture = () => React.useCallback(
+  //   () => {
+  //     const imageSrc = this.state.webcamRef.current.getScreenshot();
+  //   },
+  //   [this.state.webcamRef]
+  // );
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			webcamEnabled: false,
+			facingMode: { exact: "user"}
+		};
+	}
+
+
+	render() {
+	  return (
+	    <div className="App" style={{padding: 10}}>
+				<div className="container has-text-centered">
+					<div className="title">WebCam</div>
+					<div className="box">
+					<div> Click the button to enable your device's webcam within the browser! </div>
+			      {this.state.webcamEnabled ? (
+							<Webcam
+								audio={false}
+				        height={720}
+				        // ref={this.webcamRef}
+				        screenshotFormat="image/jpeg"
+				        width={1280}
+								videoConstraints={this.state.facingMode}
+							 />
+						) : (
+							<button type="button" onClick={this.enableWebcam}>
+							Enable Webcam
+							</button>
+						)}
+						<br />
+						<button className="has-text-centered" type="button" onClick={this.switchCamera}>
+						Switch camera
+						</button>
+						<br />
+
+					</div>
+				</div>
+	    </div>
+	  );
+	}
 }
 
 export default App;

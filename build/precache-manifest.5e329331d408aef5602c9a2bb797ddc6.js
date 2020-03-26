c406aab9431e2c934680defce114730ebf8b8899
scotch115 <jordangamache115@gmail.com>
2020-03-26 15:57:45 -0400

    Minor UI enhancements

---
+++ build/precache-manifest.5e329331d408aef5602c9a2bb797ddc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7766eee2dea93bdb1a6cc7283f4015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96fef27aada7432df22",
+    "url": "/static/css/main.bf1627ad.chunk.css"
+  },
+  {
+    "revision": "b11bac8269b29173ca4c",
+    "url": "/static/js/2.eaf48a24.chunk.js"
+  },
+  {
+    "revision": "881b4cd60baf4ebc476fd3c43b040b40",
+    "url": "/static/js/2.eaf48a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96fef27aada7432df22",
+    "url": "/static/js/main.05978034.chunk.js"
+  },
+  {
+    "revision": "6f852ea5c0fae2078df5",
+    "url": "/static/js/runtime-main.0b8d7e57.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -4,58 +4,74 @@ import './App.css';
 
 class App extends Component {
 	enableWebcam = () => this.setState({ webcamEnabled: true });
-	switchCamera = () => {
-		if (this.state.facingMode === "user") {
-			this.setState({ facingMode: "environment" });
-		} else if (this.state.facingMode === "environment") {
-			this.setState({ facingMode: "user" });
-		}
+
+
+	setRef = (webcam) => {
+		this.webcam = webcam;
 	}
 
-	// webcamRef = () => React.useRef(null);
-	// capture = () => React.useCallback(
-  //   () => {
-  //     const imageSrc = this.state.webcamRef.current.getScreenshot();
-  //   },
-  //   [this.state.webcamRef]
-  // );
+	capture = () => {
+      const imageSrc = this.webcam.getScreenshot();
+			this.setState({
+				imageData: imageSrc
+			})
+    }
 
 	constructor(props) {
 		super(props);
 		this.state = {
 			webcamEnabled: false,
-			facingMode: { exact: "user"}
+			facingMode: "user",
+			imageData: null
 		};
 	}
 
 
 	render() {
+		const videoConstraints = {
+			width: 1280,
+			height: 720,
+			facingMode: this.state.facingMode
+		}
 	  return (
-	    <div className="App" style={{padding: 10}}>
+	    <div className="App" style={{padding: 10, background: 'linear-gradient(160deg, rgb(15, 32, 39, 1), rgb(32, 58, 67, 1), rgba(44, 83, 100, 1))', width: '100vw', height: '100vh'}}>
 				<div className="container has-text-centered">
-					<div className="title">WebCam</div>
-					<div className="box">
+					<div className="title" style={{  color: 'white'}}>WebCam</div>
+					<div className="container box">
 					<div> Click the button to enable your device's webcam within the browser! </div>
+					<br />
 			      {this.state.webcamEnabled ? (
-							<Webcam
-								audio={false}
-				        height={720}
-				        // ref={this.webcamRef}
-				        screenshotFormat="image/jpeg"
-				        width={1280}
-								videoConstraints={this.state.facingMode}
-							 />
+							<div>
+								<Webcam
+									audio={false}
+					        height={720}
+					        ref={this.setRef}
+					        screenshotFormat="image/jpeg"
+					        width={1280}
+									mirrored="true"
+									videoConstraints={videoConstraints}
+								 />
+								 <button className="has-text-centered button is-small is-rounded is-warning" type="button"onClick={()=> this.state.facingMode === "user" ? this.setState({ facingMode: "environment"}) : this.setState({facingMode: "user"})}>
+	 	 						Switch camera
+	 	 						</button>
+								<br />
+								<br />
+	 	 						<button className="has-text-centered button is-small is-rounded is-link" type="button" onClick={this.capture}>
+	 	 						Take a Screenshot!
+	 	 						</button>
+		 						{this.state.imageData ?
+		 							<div>
+		 								<p><img src={this.state.imageData} alt=""/></p>
+
+		 							</div>
+		 							: null
+		 						}
+							</div>
 						) : (
-							<button type="button" onClick={this.enableWebcam}>
+							<button className="button is-small is-rounded is-info" type="button" onClick={this.enableWebcam}>
 							Enable Webcam
 							</button>
 						)}
-						<br />
-						<button className="has-text-centered" type="button" onClick={this.switchCamera}>
-						Switch camera
-						</button>
-						<br />
-
 					</div>
 				</div>
 	    </div>
